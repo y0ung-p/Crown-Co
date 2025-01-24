d08d52a2c676c25ec547432a4695a9c56be06acf
y0ung-p <132069382+y0ung-p@users.noreply.github.com>
2025-01-24 19:32:03 +1100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,16 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-auth.js";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
+  apiKey: "AIzaSyCxot6nMXg2AZywXJzIK7rgS2goMkEcMAc",
+  authDomain: "crown-co.firebaseapp.com",
+  projectId: "crown-co",
+  storageBucket: "crown-co.appspot.com",
+  messagingSenderId: "643804759346",
+  appId: "1:643804759346:web:7eb311c2e6532d04da832b",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 const auth = getAuth(app);
-
-export { db, auth };
